af7f01e0b3e5d30d77898653f330e8b5f2b4e16f
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-08 22:28:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-
-from flask import Flask, render_template
+import os
+import json
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Добро пожаловать в админ-панель EKRAN.TJ'
+    return render_template('catalog.html')
 
+@app.route('/orders', methods=['GET'])
+def get_orders():
+    with open('orders.json', 'r', encoding='utf-8') as file:
+        orders = json.load(file)
+    return jsonify(orders)
+
+@app.route('/orders', methods=['POST'])
+def save_order():
+    data = request.json
+    if not data:
+        return jsonify({'status': 'error', 'message': 'Нет данных'}), 400
+
+    try:
+        with open('orders.json', 'r', encoding='utf-8') as file:
+            orders = json.load(file)
+    except FileNotFoundError:
+        orders = []
+
+    orders.append(data)
+
+    with open('orders.json', 'w', encoding='utf-8') as file:
+        json.dump(orders, file, ensure_ascii=False, indent=4)
+
+    return jsonify({'status': 'ok'})
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
