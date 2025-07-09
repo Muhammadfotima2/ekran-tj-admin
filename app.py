48b7804901c1509da7288b52443a99f4b4e7ae65
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 18:40:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+import os
+import json
+from flask import Flask, render_template, request, jsonify
 
-app = Flask(name)
+app = Flask(__name__)  # ✅ Исправлено здесь
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('catalog.html')
 
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
+@app.route('/orders', methods=['GET'])
+def get_orders():
+    with open('orders.json', 'r', encoding='utf-8') as file:
+        orders = json.load(file)
+    return jsonify(orders)
 
-if name == 'main':
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
