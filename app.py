fbe415d4b09997843849ed910c50ee1d4ea9a48a
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-09 19:22:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-import os
-import json
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)  # ✅ Исправлено здесь
+app = Flask(name, static_folder='public')
 
 @app.route('/')
-def index():
-    return render_template('catalog.html')
+def admin_panel():
+    return send_from_directory('public', 'admin.html')
 
-@app.route('/orders', methods=['GET'])
-def get_orders():
-    with open('orders.json', 'r', encoding='utf-8') as file:
-        orders = json.load(file)
-    return jsonify(orders)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
-
+if name == 'main':
+    app.run()
