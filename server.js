8f834ec0264f529a27f4d98ca7fffefe0896801f
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-22 21:44:11 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const BOT_TOKEN = 'твой_токен';
+const CHAT_ID = 'твой_chat_id';
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.post('/order', async (req, res) => {
+  const { product, price, quantity, total, user } = req.body;
+
+  const text = 
+🛍 <b>Новый заказ из Telegram WebApp</b>
+👤 <b>Пользователь:</b> ${user.first_name} (@${user.username})
+🆔 <b>ID:</b> ${user.id}
+📱 <b>Модель:</b> ${product}
+💰 <b>Цена:</b> ${price} x ${quantity}
+📦 <b>Итого:</b> ${total} сомонӣ
+  ;
+
+  try {
+    await axios.post(https://api.telegram.org/bot${BOT_TOKEN}/sendMessage, {
+      chat_id: CHAT_ID,
+      text,
+      parse_mode: "HTML"
+    });
+
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error('❌ Ошибка Telegram:', err.message);
+    res.status(500).json({ error: 'Ошибка Telegram' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(🚀 Сервер на порту ${PORT});
+});
